326497c1acfa29e9e2f6666ca6accc67e343e609
Goribesh <43958978+Goribesh@users.noreply.github.com>
2018-10-10 13:22:24 +0200

    aggiunta libreria newmat

---
+++ Finite/Finite/differenze_implicite.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<cmath>
+#include<vector>
+#include<algorithm> 
+#include<iostream>
+#include <newmat.h>
+using namespace RBD_LIBRARIES;
+using namespace std;
+
+
+
+
+class differenze_implicite
+{
+public:
+	double option_price_put_european_finite_diff_implicit(const double& S,
+		const double& X,
+		const double& r,
+		const double& sigma,
+		const double& time,
+		const int& no_S_steps,
+		const int& no_t_steps);
+
+
+	double option_price_put_american_finite_diff_implicit(const double& S,
+		const double& K,
+		const double& r,
+		const double& sigma,
+		const double& time,
+		const int& no_S_steps,
+		const int& no_t_steps);
+};
+
