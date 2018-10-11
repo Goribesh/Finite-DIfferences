5ab6098387c52bbd0bef6a922ef80a40e2579ace
Goribesh <43958978+Goribesh@users.noreply.github.com>
2018-10-11 22:59:13 +0200

    classi padre e figlio

---
+++ Finite/Finite/ClassePadreDifferenze.cpp
@@ -0,0 +1,9 @@
+#include "ClassePadreDifferenze.h"
+
+
+
+ClassePadreDifferenze::ClassePadreDifferenze()
+{
+};
+
+
---
+++ Finite/Finite/ClassePadreDifferenze.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<vector>
+using namespace std;
+
+
+int righe1 = 0;
+
+class ClassePadreDifferenze
+{
+public:
+	ClassePadreDifferenze();
+
+	
+private:
+
+	vector<double>  S();
+	vector<double>  K();
+	vector<double> r();
+	vector<double>  sigma();
+	vector<double>  time();
+	vector<int>  no_S_steps();
+	vector<int>  no_t_steps();
+
+
+
+
+
+
+
+
+};
+
